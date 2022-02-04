d7d1a0615e4c70d9dd3ac2e95cbf049a481e34b7
Keshav <keshav7aug@gmail.com>
2022-02-04 20:35:27 +0530

    Added GUI

--- wordle.py
+++ wordle.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk.corpus import words
-import tkinter
+import tkinter as tk
 Ewords = [word.lower() for word in words.words() if len(word)==5]
 
 def getWords(words,p,w):
@@ -35,7 +35,7 @@ def getPossibleWords(X,C,Cp,Xp):
             Ewords = getWords(Ewords,i,Cp[i])
         if Xp[i]!="":
             Ewords = getWordsNot(Ewords,i,Xp[i])
-    print(Ewords)
+    return list(set(Ewords))
 def wordsToTry(X,C,Cp,Xp):
     all_words = X+C
 def main():
@@ -45,5 +45,80 @@ def main():
     Xp = ["","","","",""]
     # Cp = [""]*5
     getPossibleWords(X,C,Cp,Xp)
+class UserInterface:
+    def __init__(self):
+        self.N = 5
+        self.allPossibleWords = []
+        self.bestGuesses = []
+        self.selected = [" "]*self.N
+        self.dropDown = [" "]*self.N
+        self.notSure = [" "]*self.N
+        self.entry = [" "]*self.N
+        self.notPresent = ""
+        self.window = tk.Tk()
+        self.window.title("Wordle Helper")
+        self.window.geometry("480x480")
+    def showPossibleWords(self):
+        print("Hi")
+        CP = []
+        XP = []
+        for i in self.selected:
+            word = i.get().strip().lower()
+            if len(word) == 1:
+                CP.append(word)
+            else:
+                CP.append("")
+        for i in self.notSure:
+            word = i.get().strip().lower()
+            if len(word)>0:
+                XP.append(word)
+            else:
+                XP.append("")
+        C = "".join(CP) + "".join(XP)
+        X = []
+        if self.notPresent.get() is not None:
+            X = self.notPresent.get().strip().lower()
+        wordList = getPossibleWords(X,C,CP,XP)[0:40]
+        print(X,C,CP,XP,sep=" || ")
+        allWords = tk.Text(self.window,width=7)
+        for i,word in enumerate(wordList):
+            allWords.insert(tk.END, f"{word}\n")
+        allWords.grid(row = 7, column = 0, rowspan=len(wordList))
+
+    def showWordsTobeEntered(self):
+        print("Bo")
+        self.allPossibleWords = []
+        self.bestGuesses =  ["Words"]
+
+    def GUI(self):
+        options = ["Select"] + [f"  {chr(ord('A')+i)}  " for i in range(26)]
+        l1 = tk.Label(self.window, text = "Enter words with positions certained")
+        l1.grid(row=0, column = 1, sticky = tk.W, pady = 2, columnspan=5)
+        for i in range(self.N):
+            self.selected[i] = tk.StringVar()
+            self.selected[i].set("Select")
+            self.dropDown[i] = tk.OptionMenu(self.window,self.selected[i],*options)
+            self.dropDown[i].grid(row=1, column = i, sticky = tk.W, pady = 2)
+        l1 = tk.Label(self.window, text = "Enter words present, but not in this position")
+        l1.grid(row=2, column = 1, sticky = tk.W, pady = 2, columnspan=5)
+        for i in range(5):
+            self.notSure[i] = tk.StringVar()
+            self.notSure[i].set(" ")
+            self.entry[i] = tk.Entry(self.window, textvariable=self.notSure[i], width=5)
+            self.entry[i].grid(row = 3, column = i, sticky = tk.W, pady = 2)
+        l2 = tk.Label(self.window, text = "Enter words which are not present")
+        l2.grid(row=5, column = 0, sticky = tk.W, pady = 2, columnspan=3)
+        self.notPresent = tk.StringVar()
+        self.notPresent.set(" ")
+        notPresentEntry = tk.Entry(self.window, textvariable=self.notPresent)
+        notPresentEntry.grid(row = 5, column = 3, sticky = tk.W, pady = 2, columnspan=5)
+        submit = tk.Button(self.window, text = "Show Possible Words", command = self.showPossibleWords)
+        submit.grid(row=6, column = 0, sticky =tk.W, pady = 2, columnspan=3)
+
+        submit1 = tk.Button(self.window, text = "Show Word to Guess", command = self.showWordsTobeEntered)
+        submit1.grid(row=6, column = 3, sticky =tk.W, pady = 2, columnspan=3)
+        
+        self.window.mainloop()
+UserInterface().GUI()
 
     
